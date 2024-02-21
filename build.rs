daf875051e328948f438796f5ac5585ab975f408
Caoimhe <caoimhebyrne06@gmail.com>
2024-02-21 11:52:38 +0000

    build: Re-build when boot.S is changed

--- build.rs
+++ build.rs
@@ -2,11 +2,17 @@ use std::{env, error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let dir = env::var_os("CARGO_MANIFEST_DIR").ok_or(env::VarError::NotPresent)?;
-    let mut script = PathBuf::from(dir);
+
+    let mut script = PathBuf::from(dir.clone());
     script.push("linker.ld");
 
     println!("cargo:rustc-link-arg-bins={}", script.display());
     println!("cargo:rerun-if-changed={}", script.display());
+
+    let mut asm = PathBuf::from(dir);
+    asm.push("src/boot/boot.S");
+
+    println!("cargo:rerun-if-changed={}", asm.display());
 
     Ok(())
 }
